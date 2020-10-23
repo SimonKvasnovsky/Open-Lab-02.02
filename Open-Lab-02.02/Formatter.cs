5f690ee40a98fe66d5681811c9eb984c6cb2bac6
Simon <simon.kvasnovsky@gmail.com>
2020-10-23 19:49:19 +0200

    Function that formats double array.

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using NUnit.Common;
+using System;
 
 namespace Open_Lab_02._02
 {
     public class Formatter
     {
         public void Format(double[] numbers)
+
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < numbers.Length - 1; i++)
+            Console.Write(numbers[i].ToString(System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
+            String.Format("{0:0.00}", numbers);
+            Console.Write(" ");
+            {
+                Console.Write(numbers[numbers.Length - 1].ToString(System.Globalization.CultureInfo.InvariantCulture.NumberFormat));
+            }
+
         }
-    }
-}
+       
+}   }
